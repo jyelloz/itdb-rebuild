dc464a7a02ab78be17897676c6d782979ab559c3
Jordan Yelloz <jordan@yelloz.me>
2013-04-17 02:18:00 -0700

    Started adding support for copying artwork embedded inside media files to itdb track's artwork.

--- itdb_rebuild.py
+++ itdb_rebuild.py
@@ -6,18 +6,37 @@ _log = logging.getLogger(__name__)
 
 
 def get_metadata(path):
-    return mutagen.File(path, easy=True)
+    return mutagen.File(path), mutagen.File(path, easy=True)
 
 
-def is_compilation(md):
+def get_first(d, key, default=None):
+    try:
+        return d.get(key)[0].encode('utf-8')
+    except:
+        return default
 
-    _log.debug('%r', md)
 
-    """
+def get_any_artwork(md_hard):
+
+    try:
+        return md_hard['covr'][0]
+    except:
+        pass
+
+    try:
+        return md_hard['APIC:'].data
+    except:
+        pass
+
+    return None
+
+
+def is_compilation(md_easy):
+
     try:
         compilation = bool(
             int(
-                md.get('compilation')
+                md_easy.get('compilation', [0])[0]
             )
         )
 
@@ -26,19 +45,18 @@ def is_compilation(md):
     except:
         pass
 
-    performer = md.get('performer')
-    albumartist = md.get('albumartist')
-    artist = md.get('artist')
+    performer = md_easy.get('performer')
+    albumartist = md_easy.get('albumartist')
+    artist = md_easy.get('artist')
 
     if performer is not None and artist is not None:
         return performer[0] != artist[0]
 
     if albumartist is not None and artist is not None:
         return artist[0] != albumartist[0]
-    """
 
     try:
-        album_type = md.get('musicbrainz_albumtype')[0]
+        album_type = get_first(md_easy, 'musicbrainz_albumtype')
         return album_type == u'soundtrack' or album_type == u'compilation'
     except:
         return False
@@ -66,11 +84,35 @@ def rebuild(mountpoint, ipod_name, dry_run=True):
     gpod.itdb_playlist_set_mpl(master)
     gpod.itdb_playlist_add(db, master, -1)
 
-    def get_first(d, key, default=None):
-        try:
-            return d.get(key)[0].encode('utf-8')
-        except:
-            return default
+    mb_albumid_to_artwork = dict()
+
+    def store_artwork(artwork_data, mb_albumid):
+
+        import gio
+        from gtk import gdk
+
+        artwork_in = gio.memory_input_stream_new_from_data(artwork_data)
+
+        pixbuf = gdk.pixbuf_new_from_stream(
+            artwork_in,
+            None,
+        )
+
+        artwork = gpod.itdb_artwork_new()
+
+        gpod.itdb_artwork_set_thumbnail_from_pixbuf(
+            artwork,
+            pixbuf,
+            0,
+            None,
+        )
+
+        mb_albumid_to_artwork[mb_albumid] = artwork
+
+        return artwork
+
+    def get_artwork(mb_albumid):
+        return mb_albumid_to_artwork.get(mb_albumid)
 
     def action(path):
 
@@ -82,29 +124,58 @@ def rebuild(mountpoint, ipod_name, dry_run=True):
         relative_path = relpath(path, mountpoint)
         ipod_path = ':' + relative_path.replace(sep, ':')
 
-        md = get_metadata(path)
-        info = md.info
-        c = is_compilation(md)
+        md_hard, md_easy = get_metadata(path)
+        info = md_easy.info
+        c = is_compilation(md_easy)
 
         track = gpod.itdb_track_new()
 
-        track.title = get_first(md, 'title')
-        track.artist = get_first(md, 'artist')
-        track.album = get_first(md, 'album')
+        track.title = get_first(md_easy, 'title')
+        track.artist = get_first(md_easy, 'artist')
+        track.album = get_first(md_easy, 'album')
         track.compilation = c
         track.tracklen = int(info.length * 1000)
         track.bitrate = int(info.bitrate)
         track.samplerate = int(info.sample_rate)
         track.ipod_path = ipod_path
         track.size = stat(path).st_size
+
         if isinstance(info, M4AInfo):
             track.filetype = 'M4A-file'
         else:
             track.filetype = 'MP3-file'
 
+        mb_albumid = get_first(md_easy, 'musicbrainz_albumid', None)
+        if mb_albumid is not None:
+
+            existing_artwork = get_artwork(mb_albumid)
+            if existing_artwork is not None:
+                _log.debug(
+                    'found existing artwork for track %r (%r-%r)',
+                    path,
+                    get_first(md_easy, 'artist'),
+                    get_first(md_easy, 'album'),
+                )
+                artwork = existing_artwork
+            else:
+                artwork_data = get_any_artwork(md_hard)
+                if artwork_data is not None:
+                    _log.debug(
+                        'storing artwork for track %r',
+                        path,
+                    )
+                    artwork = store_artwork(artwork_data, mb_albumid)
+                else:
+                    artwork = None
+        else:
+            artwork = None
+
+        if artwork is not None:
+            track.artwork = gpod.itdb_artwork_duplicate(artwork)
+
         try:
-            track_number = get_first(md, 'tracknumber')
-            disc_number = get_first(md, 'discnumber', '1')
+            track_number = get_first(md_easy, 'tracknumber')
+            disc_number = get_first(md_easy, 'discnumber', '1')
 
             track_n = track_number.split('/')
             disc_n = disc_number.split('/')
@@ -119,7 +190,7 @@ def rebuild(mountpoint, ipod_name, dry_run=True):
                 track.cds = int(disc_n[1])
 
         except Exception, e:
-            _log.error('%r %r', e, md)
+            _log.error('%r %r', e, md_easy)
 
         gpod.itdb_track_add(db, track, -1)
         gpod.itdb_playlist_add_track(master, track, -1)
@@ -141,7 +212,7 @@ def main():
 
     from argparse import ArgumentParser
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
 
     parser = ArgumentParser()
 
